dcfc14404eebd141bd6b7142fe42510864004d19
Blitz <johnfloydmarticio@proton.me>
2025-09-23 01:10:10 +0800

    Minor change: fixed routes

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,10 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.send("✅ TrackJob API is running");
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
