309d7387fa9d2269665e753b1f647b52e0fd990e
Blitz <johnfloydmarticio@proton.me>
2025-10-21 01:11:12 +0800

    Minor change: add trust proxy on index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ import emailRoutes from "./routes/emailRoutes";
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 // Security middleware
 app.use(helmet());
 app.use(cors({ origin: process.env.FRONTEND_URL || "*" }));
